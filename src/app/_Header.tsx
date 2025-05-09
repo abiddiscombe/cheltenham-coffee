f248cb176895f97a86b5dd88b5cc4789ec932650
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2025-05-09 18:11:27 +0100

    feat: Increase header minimum height.

--- src/app/_Header.tsx
+++ src/app/_Header.tsx
@@ -3,7 +3,7 @@ import HeaderOverview from "./_HeaderOverview";
 
 export default function Header() {
   return (
-    <header className="bg-white border-b border-b-primary-200 px-8 py-0.5 min-h-12 flex gap-4 items-center justify-between">
+    <header className="bg-white border-b border-b-primary-200 px-8 py-0.5 min-h-14 flex gap-4 items-center justify-between">
       <Typography variant="h1" className="grow">
         Cheltenham Coffee
       </Typography>
