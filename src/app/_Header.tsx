660ec021f8dab2bb47a24ec0c7cbefadccc16ff3
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2025-05-10 13:23:28 +0100

    feat: Add Beta tag to header.

--- src/app/_Header.tsx
+++ src/app/_Header.tsx
@@ -1,12 +1,15 @@
+import Tag from "@/components/Tag";
 import Typography from "@/components/Typography";
 import HeaderOverview from "./_HeaderOverview";
 
 export default function Header() {
   return (
     <header className="bg-white border-b border-b-primary-200 px-4 sm:px-6 py-0.5 min-h-14 flex gap-4 items-center justify-between">
-      <Typography variant="h1" className="grow">
+      <Typography variant="h1" className="font-semibold">
         Cheltenham Coffee
       </Typography>
+      <Tag>Beta</Tag>
+      <hr className="border-none grow" />
       <HeaderOverview />
     </header>
   );
---
+++ src/components/Tag.tsx
@@ -0,0 +1,31 @@
+import { twMerge } from "tailwind-merge";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const cvaTag = cva("block px-2.5 py-0.5 shrink-0 rounded text-sm", {
+  variants: {
+    variant: {
+      red: "bg-red-200 text-red-700",
+      gray: "bg-gray-200 text-gray-700",
+      blue: "bg-blue-200 text-blue-700",
+      green: "bg-green-200 text-green-700",
+      orange: "bg-orange-200 text-orange-700",
+    },
+  },
+  defaultVariants: {
+    variant: "gray",
+  },
+});
+
+export default function Tag(
+  p: React.HTMLAttributes<HTMLElement> & VariantProps<typeof cvaTag>,
+) {
+  const classes = twMerge(
+    cvaTag({ variant: p.variant, className: p.className }),
+  );
+
+  return (
+    <span {...p} className={classes}>
+      {p.children}
+    </span>
+  );
+}
